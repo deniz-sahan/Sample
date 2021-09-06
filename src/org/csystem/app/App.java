7982fd09673ec951b6381db8aec9f6f9ffd0776c
deniz-sahan <ddenizsahan@gmail.com>
2021-09-06 21:32:48 +0300

    initial commit - CSD MAY-2021 course

---
+++ src/org/csystem/app/App.java
@@ -0,0 +1,16 @@
+/*---------------------------------------------------------------------------------------------------------------------- 	 
+	Aşağıdaki durumda referansa ildeğer verilmediği diziye ilkdeğer verirken new operatörü kullanılmalıdır
+----------------------------------------------------------------------------------------------------------------------*/
+package org.csystem.app;
+
+class App {
+	public static void main(String [] args) 
+	{
+		int [] a ={1, 2, 3, 4, 5, 6, 9, 7 }; //error
+
+		for (int i = 0; i < a.length; ++i)
+			System.out.printf("%d ", a[i]);
+	}
+}
+
+
